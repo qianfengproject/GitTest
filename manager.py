41ecb00d316faf89a3d9eab6d99f68f4c98246b8
ZnnZkx <znnya@sina.com>
2018-10-15 18:13:52 +0800

    更新  by:zkx

--- manager.py
+++ manager.py
@@ -1,13 +1,9 @@
 from flask_migrate import MigrateCommand
 from flask_script import Manager
-
 from APP import create_app
 
 app = create_app()
-
-
 manager = Manager(app=app)
-
 manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
