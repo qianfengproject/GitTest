3dde72f5f7203b010851a475a73baee000c2e706
ZnnZkx <znnya@sina.com>
2018-10-15 18:17:59 +0800

    api更新  by:zkx

--- APP/apis.py
+++ APP/apis.py
@@ -1,7 +1,21 @@
 from flask import Blueprint
 
+from APP.ext import db
+from APP.models import Cat
+
 blue = Blueprint('blue',__name__)
+def init_blue(app):
+    app.register_blueprint(blueprint=blue)
 
 @blue.route('/index/')
 def index():
     return 'O(∩_∩)O哈哈~'
+
+#添加：
+@blue.route('/add/')
+def add():
+    cat = Cat()
+    cat.name = 'Tom猫'
+    db.session.add(cat)
+    db.session.commit()
+    return "添加成功"
